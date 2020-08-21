4608a8e09ed6657ece3e12625e1edef8c8332118
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-08-21 13:08:28 -0400

    Creating react app with typescript and setting up index

---
+++ server/client/src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import "bootstrap/dist/css/bootstrap.css";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
